dff1f122052e7167e4ca8635187b9f1c1dffe361
ribakmasude <beranjusihombing1@gmail.com>
2023-01-31 16:53:28 +0700

    setup sign up view

---
+++ app/src/main/java/com/beranju/tokomakanan/ui/auth/signup/SignUpAddressFragment.kt
@@ -0,0 +1,19 @@
+package com.beranju.tokomakanan.ui.auth.signup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.beranju.tokomakanan.R
+
+class SignUpAddressFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up_address, container, false)
+    }
+}
